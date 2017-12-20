efeb2fc9394ac30f6ca67c7efe16b98193beb22b
Jeremy Wright <chinchalupa@gmail.com>
2017-12-20 14:34:01 -0600

    Swap

--- src/color.cpp
+++ src/color.cpp
@@ -1,1 +1,85 @@
 #include "color.h"
+
+namespace colors
+{
+    Color::Color(float r, float g, float b)
+    {
+        red = r;
+        green = g;
+        blue = b;
+    }
+
+    void Color::SetRed(float r)
+    {
+        red = r;
+        HSVNeedReset = true;
+    }
+    float Color::GetRed()
+    {
+        return red;
+    }
+    void Color::SetBlue(float b)
+    {
+        blue = b;
+        HSVNeedReset = true;
+    }
+    float Color::GetBlue()
+    {
+        return blue;
+    }
+    void Color::SetGreen(float g)
+    {
+        green = g;
+        HSVNeedReset = true;
+    }
+    float Color::GetGreen()
+    {
+        return blue;
+    }
+
+    float Color::GetHue()
+    {
+        return h;
+    }
+
+    void Color::SetHue(float h)
+    {
+        float minimum, maximum, delta;
+
+        // Get minimum
+        minimum = min(green, red);
+        minimum = min(minimum, blue);
+
+        // Get maximum
+        maximum = max(green, red);
+        maximum = max(maximum, blue);
+
+        delta = maximum - minimum;
+
+        v = maximum;
+        if (maximum != 0)
+        {
+            s = delta / maximum;
+        }
+        else
+        {
+            s = 0;
+            h = -1;
+            return;
+        }
+        if (red == maximum) h = (green - blue) / delta;
+        else if (green == maximum) h = 2 + (blue - red) / delta;
+        else h = 4 + (red - green) / delta;
+        h *= 60;
+
+        if (h < 0)
+        {
+            h += 360;
+        }
+    }
+
+    void RecomputeHSV()
+    {
+        
+    }
+}
--- src/color.h
+++ src/color.h
@@ -1,15 +1,32 @@
-class Color
+#include "application.h"
+
+namespace colors
 {
+    class Color
+    {
     private:
         float red, green, blue;
         float h;
         float s, v;
+        bool HSVNeedReset;
+
+        void RecomputeHSV();
+        void RecomputeRGB();
+
     public:
-        Color(float r, float g, float b) : red(r), green(g), blue(b) {};
+        Color(float r, float g, float b);
         float GetRed();
+        void SetRed(float r);
         float GetGreen();
+        void SetGreen(float g);
         float GetBlue();
+        void SetBlue(float b);
         float GetHue();
+        void SetHue(float h);
         float GetSaturation();
+        void SetSaturation(float s);
         float GetValue();
-};
+        void SetValue(float v);
+        float *RGBAsArray();
+    };
+}
